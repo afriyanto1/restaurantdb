bab8d8ae81a3efb51e82c0ca64a49e17a3a0dd85
Afri Yanto <afri.yanto@asyx.com>
2025-03-22 06:57:19 +0700

    Update github automation workflow

--- RestaurantProject/customerSide/customerLogin/logout.php
+++ RestaurantProject/customerSide/customerLogin/logout.php
@@ -43,7 +43,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 </head>
 <body>
     <?php
-    header("location: ../home/home.php"); // Redirect to the home page
+    header("location: ../../customerSide/home/home.php"); // Redirect to the home page
     exit;
 ?>
 </body>
